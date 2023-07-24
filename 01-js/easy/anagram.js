45e60255b5a02810dacc23968d6b3732320cfd36
Rishiveer-2003 <81288583+Rishiveer-2003@users.noreply.github.com>
2023-07-24 16:30:40 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,4 +1,5 @@
 /*
+  24/06/23
   Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
@@ -10,6 +11,11 @@
 function sortString(str) {
     return [...str].sort().join('');
 }
+/*
+the line return [...str].sort().join(''); takes an input string (str), converts it into an array of characters, 
+sorts the characters in lexicographic order, and then joins them back together to form the final sorted string, 
+which is returned as the output of the sortString function.
+*/
 function isAnagram(str1, str2){
     if(str1 == str2){
     return true;
